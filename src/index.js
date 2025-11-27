81c264961e258aaf855569507f7ee93a5c9df8a2
Nguyen Tuan Hung <hungquang246810@gmail.com>
2025-11-27 19:36:56 +0700

    blog frontend

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { AuthProvider } from "./context/AuthContext";
-import { BrowserRouter } from "react-router-dom"; // ADD THIS
+import { BrowserRouter } from "react-router-dom";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "./index.css";
 
